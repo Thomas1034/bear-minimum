01f0c88021f56b98f548baf04dd45d211e3df411
Thomas Kern <142808316+Thomas1034@users.noreply.github.com>
2025-04-12 21:44:44 -0400

    Start 1.19.2 setup/

--- fabric/src/main/java/com/startraveler/bearminimum/BearMinimum.java
+++ fabric/src/main/java/com/startraveler/bearminimum/BearMinimum.java
@@ -1,160 +1,23 @@
 package com.startraveler.bearminimum;
 
-import com.startraveler.bearminimum.entity.BlackBearEntity;
-import com.startraveler.bearminimum.entity.BrownBearEntity;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
-import net.fabricmc.fabric.api.loot.v3.LootTableSource;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.minecraft.core.HolderLookup;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.SpawnPlacementTypes;
-import net.minecraft.world.entity.SpawnPlacements;
-import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.monster.Creeper;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SpawnEggItem;
-import net.minecraft.world.level.levelgen.Heightmap;
-import net.minecraft.world.level.storage.loot.LootPool;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.entries.NestedLootTable;
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
-
-import java.util.function.Function;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 
 public class BearMinimum implements ModInitializer {
-
-
-    public static final EntityType<BlackBearEntity> BLACK_BEAR = Registry.register(
-            BuiltInRegistries.ENTITY_TYPE,
-            Constants.id("black_bear"),
-            EntityType.Builder.of(BlackBearEntity::new, MobCategory.CREATURE)
-                    .sized(1.4F, 1.4F)
-                    .clientTrackingRange(10)
-                    .build(ResourceKey.create(Registries.ENTITY_TYPE, Constants.id("black_bear")))
-    );
-    public static final EntityType<BrownBearEntity> BROWN_BEAR = Registry.register(
-            BuiltInRegistries.ENTITY_TYPE,
-            Constants.id("brown_bear"),
-            EntityType.Builder.of(BrownBearEntity::new, MobCategory.CREATURE)
-                    .sized(1.4F, 1.4F)
-                    .clientTrackingRange(10)
-                    .build(ResourceKey.create(Registries.ENTITY_TYPE, Constants.id("brown_bear")))
-    );
-    public static final Item BEAR_MEAT = register(
-            "bear_meat",
-            Item::new,
-            new Item.Properties().food(ModFoods.BEAR_MEAT)
-    );
-    public static final Item BROWN_BEAR_SPAWN_EGG = register(
-            "brown_bear_spawn_egg",
-            properties -> new SpawnEggItem(BROWN_BEAR, properties),
-            new Item.Properties()
-    );
-    public static final Item BLACK_BEAR_SPAWN_EGG = register(
-            "black_bear_spawn_egg",
-            properties -> new SpawnEggItem(BLACK_BEAR, properties),
-            new Item.Properties()
-    );
-    public static final Item COOKED_BEAR_MEAT = register(
-            "cooked_bear_meat",
-            Item::new,
-            new Item.Properties().food(ModFoods.COOKED_BEAR_MEAT)
-    );
-    private static final ResourceKey<LootTable> POLAR_BEAR_LOOT_TABLE_ID = ResourceKey.create(
-            Registries.LOOT_TABLE,
-            ResourceLocation.withDefaultNamespace("entities/polar_bear")
-    );
-    private static final ResourceKey<LootTable> MODIFIED_POLAR_BEAR_LOOT_TABLE_ID = Constants.key(
-            Registries.LOOT_TABLE,
-            "entities/polar_bear"
-    );
-
-    public static Item register(String name, Function<Item.Properties, Item> itemFactory, Item.Properties settings) {
-        // Create the item key.
-        ResourceKey<Item> itemKey = Constants.key(Registries.ITEM, name);
-
-        // Create the item instance.
-        Item item = itemFactory.apply(settings.setId(itemKey));
-
-        // Register the item.
-        Registry.register(BuiltInRegistries.ITEM, itemKey, item);
-
-        return item;
-    }
-
+    
     @Override
     public void onInitialize() {
-
+        
         // This method is invoked by the Fabric mod loader when it is ready
         // to load your mod. You can access Fabric and Common code in this
         // project.
 
         // Use Fabric to bootstrap the Common mod.
-        Constants.LOG.info("Hello Fabric world!");
+        // Constants.LOG.info("Hello Fabric world!");
         CommonClass.init();
-
-
-        FabricDefaultAttributeRegistry.register(BLACK_BEAR, BlackBearEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(BROWN_BEAR, BrownBearEntity.createAttributes());
-
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(entries -> {
-            entries.accept(BEAR_MEAT);
-            entries.accept(COOKED_BEAR_MEAT);
-        });
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.SPAWN_EGGS).register(entries -> {
-            entries.accept(BLACK_BEAR_SPAWN_EGG);
-            entries.accept(BROWN_BEAR_SPAWN_EGG);
-        });
-
-        LootTableEvents.MODIFY.register((ResourceKey<LootTable> id, LootTable.Builder builder, LootTableSource source, HolderLookup.Provider holderLookupProvider) -> {
-            if (POLAR_BEAR_LOOT_TABLE_ID.equals(id)) {
-                LootPool customPool = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
-                        .add(NestedLootTable.lootTableReference(MODIFIED_POLAR_BEAR_LOOT_TABLE_ID))
-                        .build();
-                builder.pool(customPool);
-            }
-        });
-
-        BiomeModifications.addSpawn(
-                BiomeSelectors.tag(BlackBearEntity.HAS_BLACK_BEAR_SPAWNS),
-                MobCategory.CREATURE,
-                BLACK_BEAR,
-                10,
-                1,
-                4
-        );
-
-        SpawnPlacements.register(
-                BLACK_BEAR,
-                SpawnPlacementTypes.ON_GROUND,
-                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules
-        );
-        BiomeModifications.addSpawn(
-                BiomeSelectors.tag(BrownBearEntity.HAS_BROWN_BEAR_SPAWNS),
-                MobCategory.CREATURE,
-                BROWN_BEAR,
-                10,
-                1,
-                4
-        );
-        SpawnPlacements.register(
-                BROWN_BEAR,
-                SpawnPlacementTypes.ON_GROUND,
-                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules
-        );
+        
+        // Some code like events require special initialization from the
+        // loader specific code.
+        ItemTooltipCallback.EVENT.register(CommonClass::onItemTooltip);
     }
 }
--- forge/src/main/java/com/startraveler/bearminimum/BearMinimum.java
+++ forge/src/main/java/com/startraveler/bearminimum/BearMinimum.java
@@ -1,153 +1,33 @@
 package com.startraveler.bearminimum;
 
-import com.startraveler.bearminimum.client.renderer.BlackBearRenderer;
-import com.startraveler.bearminimum.client.renderer.BrownBearRenderer;
-import com.startraveler.bearminimum.entity.BlackBearEntity;
-import com.startraveler.bearminimum.entity.BrownBearEntity;
-import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SpawnEggItem;
-import net.minecraft.world.level.storage.loot.LootPool;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.entries.NestedLootTable;
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.LootTableLoadEvent;
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 @Mod(Constants.MOD_ID)
 public class BearMinimum {
-    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(
-            ForgeRegistries.ENTITY_TYPES,
-            Constants.MOD_ID
-    );
-    public static final RegistryObject<EntityType<BlackBearEntity>> BLACK_BEAR = ENTITY_TYPES.register(
-            "black_bear",
-            () -> EntityType.Builder.of(BlackBearEntity::new, MobCategory.CREATURE)
-                    .sized(1.4F, 1.4F)
-                    .clientTrackingRange(10)
-                    .build(Constants.key(Registries.ENTITY_TYPE, "black_bear"))
-    );
-    public static final RegistryObject<EntityType<BlackBearEntity>> BROWN_BEAR = ENTITY_TYPES.register(
-            "brown_bear",
-            () -> EntityType.Builder.of(BlackBearEntity::new, MobCategory.CREATURE)
-                    .sized(1.4F, 1.4F)
-                    .clientTrackingRange(10)
-                    .build(Constants.key(Registries.ENTITY_TYPE, "brown_bear"))
-    );
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Constants.MOD_ID);
-    public static final RegistryObject<Item> BLACK_BEAR_SPAWN_EGG = ITEMS.register(
-            "black_bear_spawn_egg",
-            () -> new SpawnEggItem(
-                    BLACK_BEAR.get(), new Item.Properties()
-                    .setId(Constants.key(Registries.ITEM, "black_bear_spawn_egg"))
-            )
-    );
-    public static final RegistryObject<Item> BROWN_BEAR_SPAWN_EGG = ITEMS.register(
-            "brown_bear_spawn_egg",
-            () -> new SpawnEggItem(
-                    BROWN_BEAR.get(), new Item.Properties()
-                    .setId(Constants.key(Registries.ITEM, "brown_bear_spawn_egg"))
-            )
-    );
-    public static final RegistryObject<Item> BEAR_MEAT = ITEMS.register(
-            "bear_meat",
-            () -> new Item(new Item.Properties().food(ModFoods.BEAR_MEAT)
-                    .setId(Constants.key(Registries.ITEM, "bear_meat")))
-    );
-    public static final RegistryObject<Item> COOKED_BEAR_MEAT = ITEMS.register(
-            "cooked_bear_meat",
-            () -> new Item(new Item.Properties().food(ModFoods.COOKED_BEAR_MEAT)
-                    .setId(Constants.key(Registries.ITEM, "cooked_bear_meat")))
-    );
-    private static final ResourceLocation POLAR_BEAR_LOOT_TABLE_ID = ResourceLocation.fromNamespaceAndPath(
-            "minecraft",
-            "entities/polar_bear"
-    );
-    private static final ResourceKey<LootTable> MODIFIED_POLAR_BEAR_LOOT_TABLE_ID = Constants.key(
-            Registries.LOOT_TABLE,
-            "entities/polar_bear"
-    );
-
-    public BearMinimum(FMLJavaModLoadingContext context) {
-        IEventBus modBus = context.getModEventBus();
+    
+    public BearMinimum() {
+    
         // This method is invoked by the Forge mod loader when it is ready
         // to load your mod. You can access Forge and Common code in this
         // project.
-
-
+    
         // Use Forge to bootstrap the Common mod.
-        Constants.LOG.info("Hello Forge world!");
+        // Constants.LOG.info("Hello Forge world!");
         CommonClass.init();
-
-        ITEMS.register(modBus);
-        ENTITY_TYPES.register(modBus);
-
+    
+        // Some code like events require special initialization from the
+        // loader specific code.
+        MinecraftForge.EVENT_BUS.addListener(this::onItemTooltip);
+        
     }
-
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEventBusEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            EntityRenderers.register(BLACK_BEAR.get(), BlackBearRenderer::new);
-            EntityRenderers.register(BROWN_BEAR.get(), BrownBearRenderer::new);
-        }
-    }
-
-    @Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ModEventBusEvents {
-        @SubscribeEvent
-        public static void registerAttributes(EntityAttributeCreationEvent event) {
-            event.put(BearMinimum.BLACK_BEAR.get(), BlackBearEntity.createAttributes().build());
-            event.put(BearMinimum.BROWN_BEAR.get(), BrownBearEntity.createAttributes().build());
-        }
-
-        @SubscribeEvent
-        public void addCreative(BuildCreativeModeTabContentsEvent event) {
-            if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
-                event.accept(BEAR_MEAT);
-                event.accept(COOKED_BEAR_MEAT);
-            }
-            if (event.getTabKey() == CreativeModeTabs.SPAWN_EGGS) {
-                event.accept(BLACK_BEAR_SPAWN_EGG.get());
-                event.accept(BROWN_BEAR_SPAWN_EGG.get());
-            }
-        }
-    }
-
-    @Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-    public static class ForgeEventBusEvents {
-        @SubscribeEvent
-        public static void onLootTableLoad(LootTableLoadEvent event) {
-            ResourceLocation tableName = event.getName();
-
-            if (tableName.equals(POLAR_BEAR_LOOT_TABLE_ID)) {
-                LootPool customPool = LootPool.lootPool()
-                        .name(Constants.MOD_ID + "_add_bear_drops")
-                        .setRolls(ConstantValue.exactly(1))
-                        .add(NestedLootTable.lootTableReference(MODIFIED_POLAR_BEAR_LOOT_TABLE_ID))
-                        .build();
-                event.getTable().addPool(customPool);
-
-
-            }
-        }
+    
+    // This method exists as a wrapper for the code in the Common project.
+    // It takes Forge's event object and passes the parameters along to
+    // the Common listener.
+    private void onItemTooltip(ItemTooltipEvent event) {
+        
+        CommonClass.onItemTooltip(event.getItemStack(), event.getFlags(), event.getToolTip());
     }
 }
